b29896de8da65a2a734034b92795d53b568213c4
Rajat Rajput <rajputrajat@gmail.com>
2021-07-10 22:59:20 +0530

    working fine. but need to check asm now.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 #![feature(asm)]
 
 fn main() {
-    println!("Hello, world!");
+    let i: u64 = 3;
+    let o: u64;
+    unsafe {
+        asm!(
+            "mov {0}, {1}",
+            "add {0}, {number}",
+            out(reg) o,
+            in(reg) i,
+            number = const 5,
+        )
+    }
+    assert_eq!(o, 8);
 }
